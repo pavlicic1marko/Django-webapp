48742fb0a578a2eef545edc924371cc4c70d9858
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-29 11:38:21 +0100

    adding user modal

--- users/models.py
+++ users/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)  # email will be username
+    password = models.CharField(max_length=255)
+    username = None  # Django required field
+
+    USERNAME_FIELD = 'email'  # tell django we will be loging in with username
+    REQUIRED_FIELDS = []
