1b491c2f7975be6ca1989f30468c04ae764fe12a
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-29 14:04:30 +0100

    adding user serializer and implemeting register API the

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
+from .serializer import UserSerializer
+from rest_framework.response import Response
 
 
 # Create your views here.
 class RegisterView(APIView):
     def post(self, requests):
-        pass
+        serializer = UserSerializer(data=requests.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
